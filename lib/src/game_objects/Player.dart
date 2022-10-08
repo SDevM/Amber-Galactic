d465bb5c793231b17b5940fc6c4bd48458962dae
dgreenbluechip <dgreen@bluechipstrategies.com.jm>
2022-10-08 12:47:39 -0500

    added player sprite

---
+++ lib/src/game_objects/Player.dart
@@ -0,0 +1,46 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+
+import 'Sprite.dart';
+
+class Player extends StatefulWidget {
+  final double initX;
+  final double initY;
+  final double width;
+  final double height;
+  final int lives;
+  final Map<String, Bool> powerUps;
+  const Player({Key? key, required this.initX, required this.initY,required this.width, required this.height,required this.lives,  required this.powerUps}):super(key: key);
+
+  @override
+  State<Player> createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  late Sprite _sprite;
+  final Map<String, AssetImage> images ={};
+  @override
+  Widget build(BuildContext context) {
+    //not sure if the images should just be lives... 
+    _sprite = Sprite(widget.initX, widget.initY, widget.width, widget.height, images[widget.lives]!);
+    return Positioned(
+      top: _sprite.box.top,
+      left: _sprite.box.left,
+      width: _sprite.box.width,
+      height: _sprite.box.height,
+      child: Image(image: _sprite.image),
+    );
+  }
+  void move(double xOffset, double yOffset) {
+    Rect box = _sprite.box;
+    setState(() {
+      _sprite.box = Rect.fromLTWH(box.left + xOffset, box.top + yOffset, box.width, box.height);
+    });
+  }
+
+  void onCollide() {
+    // TODO Collision stuff
+  }
+}
+
