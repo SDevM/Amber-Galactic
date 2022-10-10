635878f209851f448a225ca45fa132e1897091c0
Kevin-Halll <kev4improved@gmail.com>
2022-10-10 13:03:03 -0500

    updated game time

--- lib/src/play_session/overlay.dart
+++ lib/src/play_session/overlay.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class OverlayPanel extends StatefulWidget {
   const OverlayPanel({super.key});
@@ -16,7 +17,7 @@ class _OverlayPanelState extends State<OverlayPanel> {
       child: Column(
         children: [
           SizedBox(
-            height: 10,
+            height: 30,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -67,12 +68,26 @@ class _OverlayPanelState extends State<OverlayPanel> {
 
 class GameTime extends StatefulWidget {
   const GameTime({Key? key}) : super(key: key);
-
   @override
   State<GameTime> createState() => _GameTimeState();
 }
 
 class _GameTimeState extends State<GameTime> {
+  var timer = Timer.run(() => print('done'));
+
+
+  var gameTime = "";
+  var _timer = null;
+  @override
+  void initState(){
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        gameTime = "${DateTime.now().minute}:${DateTime.now().second}";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -85,7 +100,7 @@ class _GameTimeState extends State<GameTime> {
           width: 5,
         ),
         Text(
-          '00:00',
+          '$gameTime',
           style: TextStyle(
               color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
         ),
@@ -106,11 +121,27 @@ class _LifeBarState extends State<LifeBar> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(Icons.rocket_launch, color: Colors.amberAccent,),
-        Icon(Icons.rocket_launch, color: Colors.amberAccent,),
-        Icon(Icons.rocket_launch, color: Colors.grey,),
-        Icon(Icons.close, color: Colors.white, size: 15,),
-        Text('2', style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),)
+        Icon(
+          Icons.rocket_launch,
+          color: Colors.amberAccent,
+        ),
+        Icon(
+          Icons.rocket_launch,
+          color: Colors.amberAccent,
+        ),
+        Icon(
+          Icons.rocket_launch,
+          color: Colors.grey,
+        ),
+        Icon(
+          Icons.close,
+          color: Colors.white,
+        ),
+        Text(
+          '2',
+          style: TextStyle(
+              color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        )
       ],
     );
   }
